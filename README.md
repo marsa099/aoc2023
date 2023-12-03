3b52175e9ca86660106cb34d8e90d25317189748
Martin Sandberg <martin@koderiet.dev>
2023-12-03 01:44:10 +0100

    Day 2 p2 solved

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%86/yellow)](src/day02)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -76,9 +76,9 @@ Both parts: 14.597ms
 
 ```
 Day 02
-Time part 1: 0.554ms
-Time part 2: -
-Both parts: 0.554ms
+Time part 1: 0.523ms
+Time part 2: 0.722ms
+Both parts: 1.245ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 3/50
-Total time: 15.15ms
+Total stars: 4/50
+Total time: 15.842ms
 ```
 
 <!--/RESULTS-->
--- src/day02/index.js
+++ src/day02/index.js
@@ -56,8 +56,48 @@ const part1 = (rawInput) => {
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput);
+  var numbers = [];
 
-  return;
+  var game = "";
+  input.split("\n").map((line) => {
+    //console.log(line);
+    game = line.split(/^Game (\d+): /)[1];
+
+    let rounds = line.split(/; /);
+
+    let redMax = 0;
+    let blueMax = 0;
+    let greenMax = 0;
+
+    var result = 0;
+
+    for (const round of rounds) {
+      //console.log("round: " + round);
+
+      let red = round.match(/(\d+) red/);
+      let blue = round.match(/(\d+) blue/);
+      //console.log("blue: " + blue);
+      let green = round.match(/(\d+) green/);
+
+      if (red) {
+        if (Number(red[1]) > redMax) redMax = Number(red[1]);
+      }
+      if (blue) {
+        if (Number(blue[1]) > blueMax) blueMax = Number(blue[1]);
+      }
+      if (green) {
+        if (Number(green[1]) > greenMax) greenMax = Number(green[1]);
+      }
+    }
+
+    // console.log(redMax, blueMax, greenMax);
+    // console.log(redMax * blueMax * greenMax);
+    numbers.push(redMax * blueMax * greenMax);
+  });
+
+  console.log(numbers.reduce((a, b) => Number(a) + Number(b), 0));
+
+  return numbers.reduce((a, b) => Number(a) + Number(b), 0);
 };
 
 run({
@@ -76,10 +116,14 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`,
+        expected: 2286,
+      },
     ],
     solution: part2,
   },
