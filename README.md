081dc7e2cafe257d846b3f21cfe578bda4f8d51c
Martin Sandberg <martin@koderiet.dev>
2023-12-04 15:28:36 +0100

    Day 4 p 1 finished

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
-![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%86/yellow)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
@@ -83,16 +83,16 @@ Both parts: 1.245ms
 
 ```
 Day 03
-Time part 1: 5.127ms
-Time part 2: 9223.428ms
-Both parts: 9228.554ms
+Time part 1: 3.325ms
+Time part 2: 8.847ms
+Both parts: 12.172ms
 ```
 
 ```
 Day 04
-Time part 1: -
+Time part 1: 30.094ms
 Time part 2: -
-Both parts: -
+Both parts: 30.094ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 6/50
-Total time: 9244.396ms
+Total stars: 7/50
+Total time: 58.108ms
 ```
 
 <!--/RESULTS-->
--- src/day04/index.js
+++ src/day04/index.js
@@ -1,11 +1,31 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput) => rawInput;
+const parseInput = (rawInput) => rawInput.split("\n");
 
 const part1 = (rawInput) => {
   const input = parseInput(rawInput);
 
-  return;
+  let totalScore = 0;
+
+  for (const line of input) {
+    console.log(line);
+    //console.log(line.match(/Card\s+(\d+):/)[1]);
+    let cardNo = line.match(/Card\s+(\d+):/)[1];
+    let winningNumbers = [...line.split(': ')[1].split('|')[0].matchAll(/\d+/g)].map(x => Number(x[0]))
+    let myNumbers = [...line.split(': ')[1].split('|')[1].matchAll(/\d+/g)].map(x => Number(x[0]));
+
+    console.log(`${winningNumbers} | ${myNumbers}`);
+    
+    let cardScore = 0;
+    for(let num of myNumbers) {
+      if (winningNumbers.includes(num))
+        cardScore = (cardScore == 0 ? 1 : cardScore * 2)
+    }
+    console.log(cardScore);
+    totalScore += cardScore
+  }
+
+  return totalScore;
 };
 
 const part2 = (rawInput) => {
@@ -17,10 +37,15 @@ const part2 = (rawInput) => {
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
+        expected: 13,
+      },
     ],
     solution: part1,
   },
